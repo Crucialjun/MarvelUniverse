5d5b69ed12f925ea5f622bc9a64333ee225ba845
Crucial Jun <nich.otieno@gmail.com>
2022-11-26 15:51:08 +0300

    design details page

--- app/src/main/java/com/pezesha/marveluniverse/CharacterDetailsFragment.kt
+++ app/src/main/java/com/pezesha/marveluniverse/CharacterDetailsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
@@ -33,6 +34,52 @@ class CharacterDetailsFragment : Fragment() {
         _binding = FragmentCharacterDetailsBinding.inflate(inflater,container,false)
 
         binding.textView3.text = args.character.name
+
+
+        if(args.character.description.isEmpty()){
+            binding.txtDescription.text = "No description"
+        }else{
+            binding.txtDescription.text = args.character.description
+        }
+
+
+        var comicListitems = ArrayList<String>()
+
+        for (item in args.character.comics!!.items){
+            comicListitems.add(item.name)
+        }
+
+        val comicsArrayAdapter = ArrayAdapter(requireContext(),android.R.layout.simple_list_item_1,
+            comicListitems
+        )
+
+        var seriesListitems = ArrayList<String>()
+
+        for (item in args.character.series!!.items){
+            seriesListitems.add(item.name)
+        }
+
+        val seriesArrayAdapter = ArrayAdapter(requireContext(),android.R.layout.simple_list_item_1,
+            seriesListitems
+        )
+
+        var storiesListitems = ArrayList<String>()
+
+        for (item in args.character.stories!!.items){
+            storiesListitems.add(item.name)
+        }
+
+        val storiesArrayAdapter = ArrayAdapter(requireContext(),android.R.layout.simple_list_item_1,
+            storiesListitems
+        )
+
+
+
+        binding.comicsListView.adapter = comicsArrayAdapter
+
+        binding.seriesListView.adapter = seriesArrayAdapter
+
+        binding.storiesListView.adapter = storiesArrayAdapter
 
         if(args.character.thumbnail?.extension == Extension.GIF){
             Glide.with(requireContext()).setDefaultRequestOptions(
